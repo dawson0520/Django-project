27ddee0bb7e90833c8f1ac9b06833ea46f6b638c
dawson0520 <48814139+dawson0520@users.noreply.github.com>
2020-05-15 09:38:27 +0800

    Update admin.py

--- apps/goods/admin.py
+++ apps/goods/admin.py
@@ -1,30 +1,3 @@
 from django.contrib import admin
 
 # Register your models here.
-
-class GoodsAdmin(object):
-    list_display = ["name", "click_num", "sold_num", "fav_num", "goods_num", "market_price",
-                    "shop_price", "goods_brief", "goods_desc", "is_new", "is_hot", "add_time"]
-    search_fields = ['name', ]
-    list_editable = ["is_hot", ]
-    list_filter = ["name", "click_num", "sold_num", "fav_num", "goods_num", "market_price",
-                   "shop_price", "is_new", "is_hot", "add_time", "category__name"]
-    style_fields = {"goods_desc": "ueditor"}
-
-    class GoodsImagesInline(object):
-        model = GoodsImage
-        exclude = ["add_time"]
-        extra = 1
-        style = 'tab'
-
-    inlines = [GoodsImagesInline]
-
-
-class GoodsCategoryAdmin(object):
-    list_display = ["name", "category_type", "parent_category", "add_time"]
-    list_filter = ["category_type", "parent_category", "name"]
-    search_fields = ['name', ]
-
-class HotSearchAdmin(object):
-    list_display = ["keywords", "index", "add_time"]
-
