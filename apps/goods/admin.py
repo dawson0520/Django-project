ccd7fc2cdd5245eba2698dc3e674c12f6546f4ca
dawson0520 <48814139+dawson0520@users.noreply.github.com>
2020-05-15 13:02:38 +0800

    Update admin.py

--- apps/goods/admin.py
+++ apps/goods/admin.py
@@ -10,4 +10,6 @@ class GoodsCategoryAdmin(object):
     list_display = ["name", "category_type", "parent_category", "add_time"]
     list_filter = ["category_type", "parent_category", "name"]
     search_fields = ['name', ]
-
+    
+   
+name = models.charfield(max_length=20)
