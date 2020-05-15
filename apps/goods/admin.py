db79b36a165914aa610ce09d3775428fe480db57
dawson0520 <48814139+dawson0520@users.noreply.github.com>
2020-05-15 12:44:33 +0800

    Update admin.py

--- apps/goods/admin.py
+++ apps/goods/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+class GoodsCategoryAdmin(object):
+    list_display = ["name", "category_type", "parent_category", "add_time"]
+    list_filter = ["category_type", "parent_category", "name"]
+    search_fields = ['name', ]
